c97746ec54d0280dc791b3a7db8962b6ba48d322
oofmaster69420 <57970114+oofmaster69420@users.noreply.github.com>
2021-05-27 17:14:04 -0700

    Add files via upload

---
+++ fixifileiconsd.sh
@@ -0,0 +1,14 @@
+# Script made by Joey Reinhart
+# Retina iFile icon made by meesebyte on Deviantart
+# iFile made by Carsten Heinelt
+
+echo Removing iOS 7 icons...
+rm /Applications/iFile.app/AppIcon* > /dev/null
+
+echo Removing original iOS 6 icon...
+rm /Applications/iFile.app/Icon.png > /dev/null
+
+echo Downloading HD iFile icon...
+curl -s https://i.imgur.com/ZFhor.png > /Applications/iFile.app/Icon.png
+
+echo Done.
