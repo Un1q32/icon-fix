8bc9b0242d7a11b74142c597c66fb0c2111c6ef7
oofmaster69420 <57970114+oofmaster69420@users.noreply.github.com>
2021-05-30 21:49:47 -0400

    Update fixifileiconsd.sh

--- fixifileiconsd.sh
+++ fixifileiconsd.sh
@@ -1,7 +1,7 @@
 # Script made by Joey Reinhart
 # iFile made by Carsten Heinelt
 
-echo Removing iOS 7 icons...
+echo Removing iOS 7+ icons...
 rm /Applications/iFile.app/AppIcon* > /dev/null
 
 echo Done.
