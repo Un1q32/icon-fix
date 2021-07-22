3d885b23e6ae49e05fcb61af361a481d568b2365
OldWorldOrdr <57970114+OldWorldOrdr@users.noreply.github.com>
2021-07-22 03:28:52 -0400

    Update fixmterminalicon.sh

--- fixmterminalicon.sh
+++ fixmterminalicon.sh
@@ -15,7 +15,7 @@ then
 fi
 
 echo Removing old icons...
-rm /Applications/MTerminal.app/icon* > /dev/null
+rm /Applications/MTerminal.app/icon*
 
 echo Downloading Legacy MTerminal icon...
 curl -s -k http://cydia.saurik.com/icon@2x/com.officialscheduler.mterminal.png > /Applications/MTerminal.app/icon-76.png
