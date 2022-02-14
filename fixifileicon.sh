4944f6b940aab9ce8ba6d282d926a4ce4664bade
OldWorldOrdr <57970114+OldWorldOrdr@users.noreply.github.com>
2022-02-14 00:33:16 -0500

    Update fixifileicon.sh

--- fixifileicon.sh
+++ fixifileicon.sh
@@ -1,25 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 # Script made by Joey Reinhart
 # Retina iFile icon made by meesebyte on Deviantart
 # iFile made by Carsten Heinelt
 
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root." 
-   exit
+   printf "This script must be run as root.\n" 
+   return 0
 fi
 
 if ! command -v curl &> /dev/null
 then
-    echo "cURL could not be found."
-    echo "Please install cURL from Cydia."
-    exit
+    printf "cURL could not be found.\n"
+    printf "Please install cURL from Cydia.\n"
+    return 0
 fi
 
-echo Removing old icons...
+printf "Removing old icons...\n"
 rm /Applications/iFile.app/AppIcon*
 rm /Applications/iFile.app/Icon.png
 
-echo Downloading HD iFile icon...
-curl -s -k https://i.imgur.com/ZFhor.png -o /Applications/iFile.app/Icon.png
+printf "Downloading HD iFile icon...\n"
+curl -s -k -o /Applications/iFile.app/Icon.png https://i.imgur.com/ZFhor.png
 
-echo Done.
+printf "Done.\n"
