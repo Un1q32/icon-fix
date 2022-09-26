a13e2b8372c844297c77837230370a449d374fe0
OldWorldOrdr <joey.t.reinhart@gmail.com>
2022-09-25 20:13:08 -0400

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-VERSION="4.0"
-mkdir -p io.github.oldworldordr.iconfix-$VERSION/usr/bin
-mkdir -p io.github.oldworldordr.iconfix-$VERSION/DEBIAN
-cp iconfix.sh io.github.oldworldordr.iconfix-$VERSION/usr/bin/iconfix
-cp iconfix.sh io.github.oldworldordr.iconfix-$VERSION/DEBIAN/postinst
-cp control io.github.oldworldordr.iconfix-$VERSION/DEBIAN/control
-dpkg -b io.github.oldworldordr.iconfix-$VERSION
